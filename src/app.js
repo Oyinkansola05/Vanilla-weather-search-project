f17b73ae5d39d1c79e48205d06f794f86496c30f
Oyinkansola05 <abassoyinkansola15@gmail.com>
2024-12-31 16:58:37 +0100

    Updated the index.html and added search functionality using API

--- src/app.js
+++ src/app.js
@@ -1,8 +1,21 @@
+function displayCurrentTemperature(response) {
+  let temperatureElement = document.querySelector("#current-temperature");
+  let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = response.data.city;
+}
+function searchCity(city) {
+  let apiKey = "10f93091ao9cdtf74b038641f13d04bd";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayCurrentTemperature);
+}
 function displayCity(event) {
   event.preventDefault();
   searchInputElement = document.querySelector(".search-input");
-let h1 = document.querySelector("h1")
-h1.innerHTML = searchInputElement.value
+  searchCity(searchInputElement.value);
 }
 searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", displayCity);
+
+searchCity("Lagos");
