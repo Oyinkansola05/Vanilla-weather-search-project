1d930299d2ad0b7de261094250881ce57fe7c156
Oyinkansola05 <abassoyinkansola15@gmail.com>
2025-01-02 17:43:10 +0100

    Used API to reflect the current time, humidity and wind speed

--- src/app.js
+++ src/app.js
@@ -4,7 +4,34 @@ function displayCurrentTemperature(response) {
   temperatureElement.innerHTML = Math.round(temperature);
   let h1 = document.querySelector("h1");
   h1.innerHTML = response.data.city;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
+  let humidity = Math.round(response.data.temperature.current);
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = `${humidity}%`;
+  let windSpeed = Math.round(response.data.wind.speed);
+  let windElement = document.querySelector("#wind-speed");
+  windElement.innerHTML = `${windSpeed}km/h`;
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  timeElement.innerHTML = formatDate(date);
 }
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function searchCity(city) {
   let apiKey = "10f93091ao9cdtf74b038641f13d04bd";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
